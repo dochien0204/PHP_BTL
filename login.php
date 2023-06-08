5e503d2e7134ece9b65359cc06e92eb7c86664cb
chien.doxuan <chien.doxuan@vti.com.vn>
2023-06-08 10:18:53 +0700

    Add modal for login page

--- login.php
+++ login.php
@@ -1,14 +1,13 @@
 <?php 
     include 'header.php';
 
-
+    $title = "First Login";
     if(isset($_POST['email'])){
         
-        $email = $_POST['email'];
+        $email = trim($_POST['email']);
         $password = $_POST['password'];
 
         // buoc1 so khop du lieu tu form vs db 
-
         $check = mysqli_query($conn,"SELECT * FROM customer WHERE email = '$email'");
 
         $errors;
@@ -29,17 +28,43 @@
               }
              
             } else{
-              $errors = "Sai tên đăng nhập hoặc mật khẩu";
+              $title = "Mật khẩu không chính xác";
+              include 'header-modal.php';
             }
 
         } else{
-          $errors = "Sai tên đăng nhập hoặc mật khẩu";
-        }
-        
+          $title = "Tài khoản không tồn tại";
+          include 'header-modal.php';
+        }      
     }
  ?>
 <section class="vh-100" style="background-color: #eee;">
   <div class="container h-100">
+
+  <!-- The Modal -->
+  <div class="modal fade show" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title"><?php echo "Hi title" ?></h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          <?php echo $title ?>
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-lg-12 col-xl-11">
         <div class="card text-black" style="border-radius: 25px;">
@@ -56,18 +81,18 @@
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw pr-4"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="email" id="form3Example3c" class="form-control" name="email" placeholder="Email" />
+                      <input type="email" id="form3Example3c" class="form-control" name="email" placeholder="Email" required/>
                     </div>
                   </div>
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-lock fa-lg me-3 fa-fw pr-4"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4c" class="form-control" name="password" placeholder="Nhập mật khẩu"/>
+                      <input type="password" id="form3Example4c" class="form-control" name="password" placeholder="Nhập mật khẩu" required/>
                     </div>
                   </div>
 
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button type="submit" class="btn btn-primary btn-lg">Đăng nhập</button>
+                    <button type="submit" class="btn btn-primary btn-lg" id="login">Đăng nhập</button>
                   </div>
 
                 </form>
